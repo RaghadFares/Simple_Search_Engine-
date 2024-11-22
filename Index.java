58066399d9e49f5f3f5c20f7f66c41eb2b7b4e52
raghad <raghadfarres@gmail.com>
2024-11-22 15:54:02 +0300

    Index class the updated version is here :)

--- Index.java
+++ Index.java
@@ -1,6 +1,8 @@
 public class Index {
     LinkedList<Document> allDocuments; // LinkedList to store all documents
 
+// the updated version :)
+
     // Constructor to initialize the Index
     public Index() {
         this.allDocuments = new LinkedList<>(); // Initialize the LinkedList of documents
@@ -11,6 +13,21 @@ public class Index {
         allDocuments.insert(document); // Add the document to the LinkedList
     }
 
+    public Document docWithID(int ID){
+        if(allDocuments.empty()){
+            System.out.println("this document does not exist");
+            return null;
+        }
+        allDocuments.findFirst();
+        while (!allDocuments.last()){
+            if(allDocuments.retrieve().id==ID)
+                return allDocuments.retrieve();
+            allDocuments.findNext();;
+        }
+        if(allDocuments.retrieve().id==ID)
+            return allDocuments.retrieve();
+        return null;
+    }
     // Method to display all documents in the index
     public void displayAllDocuments() {
         System.out.println("Index contains the following documents:");
@@ -23,6 +40,21 @@ public class Index {
             document.displayDocument(); // Use the Document class's displayDocument method
             allDocuments.findNext();
         }
+    }
+
+    public LinkedList<Integer> DocWithWord(String word){
+        LinkedList<Integer> Ans = new LinkedList<>();
+        if(allDocuments.empty()) return Ans;
+
+        allDocuments.findFirst();
+        while (!allDocuments.last()){
+            if(allDocuments.retrieve().words.exist(word.toLowerCase().trim()));
+            Ans.insert(allDocuments.retrieve().id);
+            allDocuments.findNext();
+        }
+        if(allDocuments.retrieve().words.exist(word.toLowerCase().trim()));
+        Ans.insert(allDocuments.retrieve().id);
+        return Ans;
     }
 }
 
